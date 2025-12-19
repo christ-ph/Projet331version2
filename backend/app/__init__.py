ae583ee3d325e3614a7bd708dc35b375a4f45694
testeurx237-beep <testeurX237@gmail.com>
2025-12-19 02:13:17 +0100

    A jour

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -39,7 +39,7 @@ def create_app():
     app.register_blueprint(profile_bp, url_prefix='/api/profiles')
     app.register_blueprint(missions_bp , url_prefix='/api/missions')
     from app.admin.routes import admin_bp
-    app.register_blueprint(admin_bp, url_prefix='/api/admin')
+    app.register_blueprint(admin_bp, url_prefix='/admin')
 
     # from app.chats.routes import chat_bp
     # app.register_blueprint(chat_bp) 
